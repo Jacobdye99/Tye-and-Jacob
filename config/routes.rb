1603c3c050584eb6feb7049e8d23ba7f0a6a2cc2
Jacobdye99 <Jacobdye99@gmail.com>
2022-04-21 18:09:16 -0400

    Fixing file structure

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  
+end
